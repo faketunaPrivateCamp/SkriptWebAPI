f53087f24f256747b4795d406fb18d5c3ebf47be
faketuna <faketuna.st@gmail.com>
2022-12-30 02:39:16 +0900

    (Fix) GETリクエスト以外のEventが動作しない問題を修正

--- src/main/java/jp/faketuna/addon/skriptwebapi/api/server/http/HttpAPIHandler.kt
+++ src/main/java/jp/faketuna/addon/skriptwebapi/api/server/http/HttpAPIHandler.kt
@@ -3,7 +3,10 @@ package jp.faketuna.addon.skriptwebapi.api.server.http
 import com.sun.net.httpserver.HttpExchange
 import com.sun.net.httpserver.HttpHandler
 import jp.faketuna.addon.skriptwebapi.SkriptWebAPI
+import jp.faketuna.addon.skriptwebapi.api.server.events.DeleteRequestEvent
 import jp.faketuna.addon.skriptwebapi.api.server.events.GetRequestEvent
+import jp.faketuna.addon.skriptwebapi.api.server.events.PostRequestEvent
+import jp.faketuna.addon.skriptwebapi.api.server.events.PutRequestEvent
 import org.bukkit.Bukkit
 
 class HttpAPIHandler: HttpHandler {
@@ -12,6 +15,21 @@ class HttpAPIHandler: HttpHandler {
         if (exchange.requestMethod == "GET"){
             Bukkit.getScheduler().callSyncMethod(SkriptWebAPI.Static.getPlugin()) {
                 Bukkit.getPluginManager().callEvent(GetRequestEvent(exchange))
+            }
+        }
+        if (exchange.requestMethod == "POST"){
+            Bukkit.getScheduler().callSyncMethod(SkriptWebAPI.Static.getPlugin()) {
+                Bukkit.getPluginManager().callEvent(PostRequestEvent(exchange))
+            }
+        }
+        if (exchange.requestMethod == "PUT"){
+            Bukkit.getScheduler().callSyncMethod(SkriptWebAPI.Static.getPlugin()) {
+                Bukkit.getPluginManager().callEvent(PutRequestEvent(exchange))
+            }
+        }
+        if (exchange.requestMethod == "DELETE"){
+            Bukkit.getScheduler().callSyncMethod(SkriptWebAPI.Static.getPlugin()) {
+                Bukkit.getPluginManager().callEvent(DeleteRequestEvent(exchange))
             }
         }
     }
