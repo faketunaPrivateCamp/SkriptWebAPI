41594168064533a73da69f63587e33fac7338651
faketuna <faketuna.st@gmail.com>
2023-12-25 22:10:16 +0900

    Feat: Now able to remove request header

---
+++ src/main/java/dev/f2a/addon/skriptwebapi/elements/request/effects/EffRemoveHeaderValue.java
@@ -0,0 +1,53 @@
+package dev.f2a.addon.skriptwebapi.elements.request.effects;
+
+import ch.njol.skript.Skript;
+import ch.njol.skript.lang.Effect;
+import ch.njol.skript.lang.Expression;
+import ch.njol.skript.lang.SkriptParser;
+import ch.njol.util.Kleenean;
+import com.google.api.client.http.HttpHeaders;
+import org.bukkit.event.Event;
+import org.jetbrains.annotations.Nullable;
+
+public class EffRemoveHeaderValue extends Effect {
+
+    static {
+        Skript.registerEffect(
+                EffRemoveHeaderValue.class,
+                "[skeb] remove [the] %httpheader%['s] (header|header properties|header prop) %string%"
+        );
+    }
+
+    private Expression<HttpHeaders> httpHeaders;
+    private Expression<String> headerKey;
+
+    @Override
+    protected void execute(Event e) {
+        HttpHeaders headers = httpHeaders.getSingle(e);
+        String key = headerKey.getSingle(e);
+
+        if(headers == null) {
+            Skript.error("Provided HttpHeaders instance is null!");
+            return;
+        }
+        if(key == null || key.isEmpty()) {
+            Skript.error("Header key is cannot be blank!");
+            return;
+        }
+
+        headers.remove(key);
+    }
+
+    @Override
+    public String toString(@Nullable Event e, boolean debug) {
+        return "";
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public boolean init(Expression<?>[] exprs, int matchedPattern, Kleenean isDelayed, SkriptParser.ParseResult parseResult) {
+        httpHeaders = (Expression<HttpHeaders>) exprs[0];
+        headerKey = (Expression<String>) exprs[1];
+        return true;
+    }
+}
