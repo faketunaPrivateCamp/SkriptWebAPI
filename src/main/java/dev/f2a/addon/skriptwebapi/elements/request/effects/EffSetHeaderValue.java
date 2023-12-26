413f8fac098843cdb7035e7168eac79049f64303
faketuna <faketuna.st@gmail.com>
2023-12-27 03:10:49 +0900

    Feat: header value is now directly Edit/Retrieve with request/response instance.

--- src/main/java/dev/f2a/addon/skriptwebapi/elements/request/effects/EffSetHeaderValue.java
+++ src/main/java/dev/f2a/addon/skriptwebapi/elements/request/effects/EffSetHeaderValue.java
@@ -6,6 +6,8 @@ import ch.njol.skript.lang.Expression;
 import ch.njol.skript.lang.SkriptParser;
 import ch.njol.util.Kleenean;
 import com.google.api.client.http.HttpHeaders;
+import com.google.api.client.http.HttpRequest;
+import com.google.api.client.http.HttpResponse;
 import org.bukkit.event.Event;
 import org.jetbrains.annotations.Nullable;
 
@@ -14,30 +16,36 @@ public class EffSetHeaderValue extends Effect {
     static {
         Skript.registerEffect(
                 EffSetHeaderValue.class,
-                "[skeb] set [the] %httpheader%['s] (header|header properties|header prop) %string% to %objects%"
+                "[skeb] set [the] %httprequest/httpresponse%['s] (header|header properties|header prop) %string% to %objects%"
         );
     }
 
-    private Expression<HttpHeaders> httpHeaders;
     private Expression<String> headerKey;
     private Expression<?> unParsedExpr;
+    private Expression<?> unParsedExprWebReqs;
 
     @Override
     protected void execute(Event e) {
-        HttpHeaders headers = httpHeaders.getSingle(e);
         String key = headerKey.getSingle(e);
         Object value = unParsedExpr.getSingle(e);
+        Object instance = unParsedExprWebReqs.getSingle(e);
 
-        if(headers == null) {
-            Skript.error("Provided HttpHeaders instance is null!");
-            return;
-        }
         if(key == null || key.isEmpty()) {
             Skript.error("Header key is cannot be blank!");
             return;
         }
 
-        headers.set(key, value);
+        if(instance instanceof HttpRequest) {
+            HttpRequest request = (HttpRequest) unParsedExprWebReqs.getSingle(e);
+            request.getHeaders().set(key, value);
+        }
+        else if(instance instanceof HttpResponse){
+            HttpResponse response = (HttpResponse) unParsedExprWebReqs.getSingle(e);
+            response.getHeaders().set(key, value);
+        }
+        else {
+            Skript.error("Provided HttpRequest or HttpResponse instance is null!");
+        }
     }
 
     @Override
@@ -48,7 +56,7 @@ public class EffSetHeaderValue extends Effect {
     @Override
     @SuppressWarnings("unchecked")
     public boolean init(Expression<?>[] exprs, int matchedPattern, Kleenean isDelayed, SkriptParser.ParseResult parseResult) {
-        httpHeaders = (Expression<HttpHeaders>) exprs[0];
+        unParsedExprWebReqs = exprs[0];
         headerKey = (Expression<String>) exprs[1];
         unParsedExpr = exprs[2];
         return true;
--- src/main/java/dev/f2a/addon/skriptwebapi/elements/request/expressions/ExprGetHeaderValue.java
+++ src/main/java/dev/f2a/addon/skriptwebapi/elements/request/expressions/ExprGetHeaderValue.java
@@ -7,6 +7,8 @@ import ch.njol.skript.lang.SkriptParser;
 import ch.njol.skript.lang.util.SimpleExpression;
 import ch.njol.util.Kleenean;
 import com.google.api.client.http.HttpHeaders;
+import com.google.api.client.http.HttpRequest;
+import com.google.api.client.http.HttpResponse;
 import org.bukkit.event.Event;
 import org.jetbrains.annotations.Nullable;
 
@@ -17,25 +19,31 @@ public class ExprGetHeaderValue extends SimpleExpression<String> {
                 ExprGetHeaderValue.class,
                 String.class,
                 ExpressionType.SIMPLE,
-                "[sekb] %httpheader%['s] header value from key %string%"
+                "[sekb] %httprequest/httpresponse%['s] header value from key %string%"
         );
     }
 
     private Expression<String> headerProperty;
-    private Expression<HttpHeaders> httpHeaders;
+    private Expression<?> unParsedExpr;
 
     @Override
     protected @Nullable String[] get(Event e) {
         String[] retr = new String[1];
-        HttpHeaders headers = httpHeaders.getSingle(e);
+        Object instance = unParsedExpr.getSingle(e);
         String headerProp = headerProperty.getSingle(e);
 
-        if(headers == null) {
-            Skript.error("Provided HttpHeaders instance is null!");
+        if(instance instanceof HttpRequest) {
+            HttpRequest request = (HttpRequest) unParsedExpr.getSingle(e);
+            retr[0] = request.getHeaders().getFirstHeaderStringValue(headerProp);
+        }
+        else if(instance instanceof HttpResponse){
+            HttpResponse response = (HttpResponse) unParsedExpr.getSingle(e);
+            retr[0] = response.getHeaders().getFirstHeaderStringValue(headerProp);
+        }
+        else {
+            Skript.error("Provided HttpRequest or HttpResponse instance is null!");
             return null;
         }
-
-        retr[0] = headers.getFirstHeaderStringValue(headerProp);
 
         return retr;
     }
@@ -58,8 +66,8 @@ public class ExprGetHeaderValue extends SimpleExpression<String> {
     @Override
     @SuppressWarnings("unchecked")
     public boolean init(Expression<?>[] exprs, int matchedPattern, Kleenean isDelayed, SkriptParser.ParseResult parseResult) {
+        unParsedExpr = exprs[0];
         headerProperty = (Expression<String>) exprs[1];
-        httpHeaders = (Expression<HttpHeaders>) exprs[0];
         return true;
     }
 }
