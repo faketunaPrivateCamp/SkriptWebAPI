a2dcd9eb1c61a2f5e840ef50de35d45e5324704b
faketuna <faketuna.st@gmail.com>
2023-12-27 03:29:04 +0900

    Feat: Now able to send response to http request

--- src/main/java/dev/f2a/addon/skriptwebapi/elements/server/effects/EffSendHttpResponse.java
+++ src/main/java/dev/f2a/addon/skriptwebapi/elements/server/effects/EffSendHttpResponse.java
@@ -9,34 +9,68 @@ import ch.njol.skript.lang.Effect;
 import ch.njol.skript.lang.Expression;
 import ch.njol.skript.lang.SkriptParser;
 import ch.njol.util.Kleenean;
+import com.sun.net.httpserver.HttpExchange;
 import org.bukkit.event.Event;
 import org.jetbrains.annotations.Nullable;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 @Name("Response to web request")
 @Description("")
 @Examples("")
 @Since("0.1.0")
 public class EffSendHttpResponse extends Effect {
-/*
+
     static {
         Skript.registerEffect(
                 EffSendHttpResponse.class,
-                "[skeb] reply %httpexchange% [to [the] request sender] [(with|and) body %-string%] [(with|and) response code %-integer%]"
+                "[skeb] [reply|response] %httpexchange% [to [the] request sender] [(with|and) body %-string%] [(with|and) response code %-integer%]"
         );
     }
-*/
+
+    private Expression<HttpExchange> httpExchange;
+    private Expression<String> responseBody;
+    private Expression<Integer> responseCode;
+
     @Override
     protected void execute(Event e) {
+        HttpExchange exchange = httpExchange.getSingle(e);
+        String body = "";
+        if(responseBody != null) {
+            body = responseBody.getSingle(e);
+        }
+        Integer code = 500;
+        if(responseCode != null) {
+            code = responseCode.getSingle(e);
+        }
 
+        if(exchange == null) {
+            Skript.error("Provided HttpExchange instance is null!");
+            return;
+        }
+
+        try {
+            exchange.sendResponseHeaders(code, body.length());
+            exchange.getResponseBody().write(body.getBytes(StandardCharsets.UTF_8));
+            exchange.close();
+        } catch (IOException ex) {
+            Skript.error("An error occurred while send response to request!");
+            throw new RuntimeException(ex);
+        }
     }
 
     @Override
     public String toString(@Nullable Event e, boolean b) {
-        return null;
+        return "";
     }
 
     @Override
+    @SuppressWarnings("unchecked")
     public boolean init(Expression<?>[] exprs, int i, Kleenean kleenean, SkriptParser.ParseResult parseResult) {
-        return false;
+        httpExchange = (Expression<HttpExchange>) exprs[0];
+        if(exprs[1] != null) responseBody = (Expression<String>) exprs[1];
+        if(exprs[2] != null) responseCode = (Expression<Integer>) exprs[2];
+        return true;
     }
 }
