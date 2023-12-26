f77bcd3488cdd9d0d1e92ee1f33599abd90ee441
faketuna <faketuna.st@gmail.com>
2023-12-26 18:23:02 +0900

    Refactor: Very simple refactor in Types.java

--- src/main/java/dev/f2a/addon/skriptwebapi/elements/Types.java
+++ src/main/java/dev/f2a/addon/skriptwebapi/elements/Types.java
@@ -6,27 +6,36 @@ import ch.njol.skript.registrations.Classes;
 import com.google.api.client.http.HttpHeaders;
 import com.google.api.client.http.HttpRequest;
 import com.google.api.client.http.HttpResponse;
+import com.sun.net.httpserver.HttpExchange;
 
-@SuppressWarnings({"unchecked", "rawtypes"})
 public class Types {
     static {
-        Classes.registerClass(new ClassInfo(HttpRequest.class, "httprequest")
+        Classes.registerClass(new ClassInfo<>(HttpRequest.class, "httprequest")
                 .name("httprequest")
                 .usage("Http request instance")
                 .user("httprequest")
-                .defaultExpression(new EventValueExpression(HttpRequest.class))
+                .defaultExpression(new EventValueExpression<>(HttpRequest.class))
         );
-        Classes.registerClass(new ClassInfo(HttpResponse.class, "httpresponse")
+
+        Classes.registerClass(new ClassInfo<>(HttpResponse.class, "httpresponse")
                 .name("httpresponse")
                 .usage("Http response instance")
                 .user("httpresponse")
-                .defaultExpression(new EventValueExpression(HttpResponse.class))
+                .defaultExpression(new EventValueExpression<>(HttpResponse.class))
         );
-        Classes.registerClass(new ClassInfo(HttpHeaders.class, "httpheader")
+
+        Classes.registerClass(new ClassInfo<>(HttpHeaders.class, "httpheader")
                 .name("httpheader")
                 .usage("Http headers instance")
                 .user("httpheader")
-                .defaultExpression(new EventValueExpression(HttpHeaders.class))
+                .defaultExpression(new EventValueExpression<>(HttpHeaders.class))
+        );
+
+        Classes.registerClass(new ClassInfo<>(HttpExchange.class, "httpexchange")
+                .name("httpexchange")
+                .usage("Http exchange instance")
+                .user("httpexchange")
+                .defaultExpression(new EventValueExpression<>(HttpExchange.class))
         );
     }
 }
